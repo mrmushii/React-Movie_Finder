2c9903d5dddbfef66d76f9bbbbe3d1ec6f6564b1
Mushfiq <mushfiq2808@gmail.com>
2025-01-27 13:42:50 +0600

    lots of work done again toooo

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -51,7 +51,7 @@ const MovieDetails = () => {
   if (errorMessage) return <p className="text-red-500">{errorMessage}</p>;
 
   return (
-    <div className="movie-card xl:px-18 sm:px-0 md:px-0 ">
+    <div className="movie-card xl:px-18 sm:px-2 md:px-2 xs:px-2 max-xs:px-1">
       <div className="flex justify-between xl:pt-12 sm:pt-0 md:pt-0 sm:px-18 md:px-18 ">
         <div>
           <h2>{movie.title}</h2>
@@ -70,70 +70,70 @@ const MovieDetails = () => {
       </div>
       <br />
       <br />
-      <div className="flex xl:flex-row  sm:flex-col md:flex-row justify-between gap-18 rounded-4xl m-20">
-      <img width={300} height={400} className="w-auto"
+      <div className="flex xl:flex-row justify-between gap-18 rounded-4xl m-20 max-sm:mx-1.5">
+      <img  className="w-auto "
         src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : "/no-movie.png"}
         alt={movie.title}
       />
        {trailerId && <MovieTrailer trailerId={trailerId} />}
 
       </div>
-      <div className="mx-20 bg-gray-800 text-white rounded-2xl shadow-lg p-6 space-y-4 max-w-screen">
-      <div>
-        <h1 className="text-2xl font-bold">{movie.title}</h1>
-        <p className="text-sm text-gray-400">{movie.tagline}</p>
-      </div>
-      <div>
-        <h2 className="font-semibold text-lg">Genres</h2>
-        <div className="flex flex-wrap gap-2">
-          {movie.genres.map((genre) => (
-            <span
-              
-              className="bg-blue-500 text-xs rounded-full px-3 py-1"
-            >
-              {genre.name}
-            </span>
-          ))}
+      <div className="mx-20 bg-gray-800 text-white rounded-2xl shadow-lg p-6 space-y-4 max-w-screen max-sm:mx-2.5">
+            <div>
+              <h1 className="text-2xl font-bold">{movie.title}</h1>
+              <p className="text-sm text-gray-400">{movie.tagline}</p>
+          </div>
+            <div>
+            <h2 className="font-semibold text-lg">Genres</h2>
+            <div className="flex flex-wrap gap-2">
+              {movie.genres.map((genre) => (
+                <span
+                  
+                  className="bg-blue-500 text-xs rounded-full px-3 py-1"
+                >
+                  {genre.name}
+                </span>
+              ))}
+            </div>
+            </div>
+            <div>
+              <h2 className="font-semibold text-lg ">Overview</h2>
+              <p className="max-w-3xl flex sm:flex-wrap">{movie.overview}</p>
+            </div>
+          <div className="grid grid-cols-2 gap-4 text-sm">
+            <div>
+              <p className="text-gray-400">Release Date</p>
+              <p>{movie.release_date}</p>
+            </div>
+            <div>
+              <p className="text-gray-400">Countries</p>
+              <p>{movie.origin_country}</p>
+            </div>
+            <div>
+              <p className="text-gray-400">Language</p>
+              <p>{movie.original_language}</p>
+            </div>
+            <div>
+              <p className="text-gray-400">Budget</p>
+              <p>${movie.budget.toLocaleString()}</p>
+            </div>
+            <div>
+              <p className="text-gray-400">Revenue</p>
+              <p>${movie.revenue.toLocaleString()}</p>
+            </div>
+            <div>
+              <p className="text-gray-400">Status</p>
+              <p>{movie.status}</p>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-semibold text-lg">Production Companies</h2>
+            <p>{movie.production_companies.map((com) =>(
+              <p>{com.name}</p>
+            ))}</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <h2 className="font-semibold text-lg ">Overview</h2>
-        <p className="max-w-3xl flex sm:flex-wrap">{movie.overview}</p>
-      </div>
-      <div className="grid grid-cols-2 gap-4 text-sm">
-        <div>
-          <p className="text-gray-400">Release Date</p>
-          <p>{movie.release_date}</p>
-        </div>
-        <div>
-          <p className="text-gray-400">Countries</p>
-          <p>{movie.origin_country}</p>
-        </div>
-        <div>
-          <p className="text-gray-400">Language</p>
-          <p>{movie.original_language}</p>
-        </div>
-        <div>
-          <p className="text-gray-400">Budget</p>
-          <p>${movie.budget.toLocaleString()}</p>
-        </div>
-        <div>
-          <p className="text-gray-400">Revenue</p>
-          <p>${movie.revenue.toLocaleString()}</p>
-        </div>
-        <div>
-          <p className="text-gray-400">Status</p>
-          <p>{movie.status}</p>
-        </div>
-      </div>
-      <div>
-        <h2 className="font-semibold text-lg">Production Companies</h2>
-        <p>{movie.production_companies.map((com) =>(
-          <p>{com.name}</p>
-        ))}</p>
-      </div>
-    </div>
-      
+          
       
       
     </div>
